a34f9df63f2e4724caf3d72e13d4229a1b7d9e0b
Bijou <bijou.abraham@technipfmc.com>
2024-03-18 06:28:23 -0700

    Fix for compilation issue on Ubuntu 24.04

--- rmw_connextdds_common/src/ndds/dds_api_ndds.cpp
+++ rmw_connextdds_common/src/ndds/dds_api_ndds.cpp
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <string>
-#include <map>
-#include <vector>
 #include <cmath>
+#include <cstdint>
+#include <map>
+#include <string>
+#include <vector>
 
 #include "rcpputils/scope_exit.hpp"
 
